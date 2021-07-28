3a165a7fa79bda29b187b4ffd605368738c322e4
EngrJoshN <joshuanyamweno@yahoo.com>
2021-07-28 03:16:16 -0700

    convert to binary

--- binary.c
+++ binary.c
@@ -1,49 +1,42 @@
 #include "holberton.h"
-
 /**
- * itob - change int to binary
- * @list: int to change
- * Return: string with binary
+ * _print_b - convert a decimal in a base 2
+ *  @flist: list of arguments int
+ *  Return: len
  */
-
-char *itob(va_list list)
+int _print_b(va_list flist)
 {
-	int j = 0, twos = 1;
-	int i, k;
-	char *s;
+	long int base = 2;
+	long int n = (long int)va_arg(flist, unsigned int);
+	int c = 1, len = 0;
+	char w = '\0';
 
-	k = va_arg(list, int);
-	i = k;
-
-	/* malloc up to max int in binary */
-	s = malloc(sizeof(char) * 33);
-	if (s == NULL)
-		return (NULL);
-
-	/* account for negative numbers with '1' at index 0 */
-	if (k < 0)
+	if (n < 2)
 	{
-		s[0] = 1 + '0';
-		j++;
-		k *= -1;
-		i *= -1;
+		w = n == 0 ? '0' : '1';
+		write(1, &w, 1);
 	}
-
-	/* find biggest power of 2 it's divisible by */
-	while (k > 1)
+	else
 	{
-		k /= 2;
-		twos *= 2;
+		while (c > 0)
+		{
+			if ((n / base) >= 2)
+			{
+				base *= 2;
+				c++;
+			}
+			else
+			{
+				w = (n / base) + '0';
+				write(1, &w, 1);
+				n %= base;
+				base /= 2;
+				len++;
+				c--;
+			}
+		}
+		w = n + '0';
+		write(1, &w, 1);
 	}
-
-	/* divide down and store binary num */
-	while (twos > 0)
-	{
-		s[j++] = (i / twos + '0');
-		i %= twos;
-		twos /= 2;
-	}
-	s[j] = '\0';
-
-	return (s);
+	return (++len);
 }
